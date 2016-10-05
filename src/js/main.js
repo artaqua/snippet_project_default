104bab6037fd9c214a6585faeb9163b37086ac38
Nicholas Krivov <artaquan@gmail.com>
2016-10-05 17:48:35 +0300

    upd

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,24 @@
 ;'use strict';
-(function() {
 
-  // Display loading...
-  $(window).load(function() {
-    $('.page-loading').addClass('active');
-  });
-  
-  $(document).ready(function() {
+// Display loading...
+$(window).load(function() {
+  $('.page-loading').addClass('active');
+});
+// DOM Ready
+$(function(){ 
 
-    // Open popup
-    $('.clickme').on('click', function(event) {
-      event.preventDefault();
-      $('.popup').addClass('active');
-    });
-
-    // Close popup
-    $('.popup .close-popup').on('click', function(event) {
-      event.preventDefault();
-      $(this).closest('.popup').removeClass('active');
-    });
-
+  // Open popup
+  $('.clickme').on('click', function(event) {
+    event.preventDefault();
+    $('.popup').addClass('active');
   });
 
-})();
+  // Close popup
+  $('.popup .close-popup').on('click', function(event) {
+    event.preventDefault();
+    $(this).closest('.popup').removeClass('active');
+  });
+
+});
 
 
